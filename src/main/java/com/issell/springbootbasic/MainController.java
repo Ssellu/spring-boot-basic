32f6374f2205b0e82daf5fbdc668b16fc8018e78
Sera Lee <ssellu.lee@gmail.com>
2021-06-30 13:52:27 +0900

    Get/Post for Gugudan

--- src/main/java/com/issell/springbootbasic/MainController.java
+++ src/main/java/com/issell/springbootbasic/MainController.java
@@ -1,7 +1,12 @@
 package com.issell.springbootbasic;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
@@ -9,5 +14,16 @@ public class MainController {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/gugudan")
+    public String gugudanForm(){
+        return "gugudan_form";
+    }
+
+    @PostMapping("/gugudan")
+    public String gugudan(int dan, Model model) {
+        model.addAttribute("dan", dan);
+        return "gugudan_result";
     }
 }
